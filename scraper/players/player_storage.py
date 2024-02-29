97ed3ba9b5c53f82d165cb6676afa056607e8cbf
greatjudge <greatjudge0@gmail.com>
2024-02-29 23:24:19 +0300

    bugfix forgot club_caps

--- scraper/players/player_storage.py
+++ scraper/players/player_storage.py
@@ -14,6 +14,7 @@ class PlayerStorage:
         player['height'] = info['height']
         player['position'] = info['position']
         player['current_club'] = info['current_club']
+        player['club_caps'] = info['club_caps']
         player['club_conceded'] = info['club_conceded']
         player['club_scored'] = info['club_scored']
         player['national_caps'] = info['national_caps']
