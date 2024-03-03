d55d404a06afd8dc8c333784aac2e908ceb2e9d7
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2024-03-03 17:54:25 +0300

    fix for Ta

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
 
     logger = logging.getLogger('Runner')
     seed_urls = [args.url]
-    #seed_urls = ['https://ru.wikipedia.org/wiki/%D0%94%D0%B8%D0%B0%D1%88,_%D0%A0%D1%83%D0%B1%D0%B5%D0%BD']
+    #seed_urls = ['https://ru.wikipedia.org/wiki/%D0%A2%D0%B0,_%D0%94%D0%B6%D0%BE%D0%BD%D0%B0%D1%82%D0%B0%D0%BD']
     parser = CssSelectorParser(logger)
     sink = FileSink(args.result_filepath)
     runner = SimpleRunner(parser, sink, logger, seed_urls)
