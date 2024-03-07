622646ce7b515a588ecb29bd9cc384170b8225e5
Timofei Pshenichnyy <timofei.psheno@gmail.com>
2024-03-07 09:54:33 +0300

    added asyncio runner from seminar

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import logging
 import time
 import argparse
+import asyncio
 
+from runners.async_runner import AsyncRunner
 from runners.simple_runner import SimpleRunner
 from parsers.css_selector_parser import CssSelectorParser
 from utils.file_sink import FileSink
@@ -22,10 +24,17 @@ def main():
     #seed_urls = ['https://ru.wikipedia.org/wiki/%D0%93%D0%B0%D0%B9%D1%8F,_%D0%A5%D0%BE%D1%81%D0%B5_%D0%9B%D1%83%D0%B8%D1%81']
     parser = CssSelectorParser(logger)
     sink = FileSink(args.result_filepath)
-    runner = SimpleRunner(parser, sink, logger, seed_urls)
-    start = time.time()
-    runner.run()
-    logger.info(f'Total duration is {time.time() - start}')
+    # runner = SimpleRunner(parser, sink, logger, seed_urls)
+    # start = time.time()
+    # runner.run()
+    # logger.info(f'Total duration is {time.time() - start}')
+
+    async def run_async_crawl():
+        runner = AsyncRunner(parser, sink, logger, seed_urls, rate=100, max_tries=5, max_parallel=5)
+        start = time.time()
+        await runner.run()
+        logger.info(f'Total duration is {time.time() - start}')
+    asyncio.run(run_async_crawl())
 
 if __name__ == '__main__':
     main()
