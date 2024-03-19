e5688ec56e0370400c3cb362776dfd4eb5ac3cb6
dayforjames <sergey.nikitin2013@gmail.com>
2024-03-19 18:43:31 +0300

    aaaaa

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,7 @@ path_to_result=$2
 
 # TODO: actual implementation should go here
 # timeout 10 wget --recursive -w 0.1 -D localhost $seed_url || true
-./venv/python main.py $seed_url $path_to_result
+venv/python main.py $seed_url $path_to_result
 
 # cp test_data/result.jsonl $path_to_result
 echo "Finished running"
