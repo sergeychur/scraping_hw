3645441b575c4c12afb0d127895040a84c85ce8b
Maksim <Maksim>
2024-04-23 04:01:07 +0300

    Updated run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,7 +13,7 @@ path_to_result=$2
 # TODO: actual implementation should go here
 timeout 10 wget --recursive -w 0.1 -D localhost $seed_url || true
 
-python3 ../main.py $seed_url
+python3 ./main.py $seed_url ./test_data/result.jsonl
 
-cp test_data/result.jsonl $path_to_result
+cp ./test_data/result.jsonl $path_to_result
 echo "Finished running"
