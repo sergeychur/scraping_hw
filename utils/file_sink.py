be684127999af33877624b0439494123836e2cfb
Maksim <Maksim>
2024-04-23 23:38:08 +0300

    chenges in typing

--- utils/file_sink.py
+++ utils/file_sink.py
@@ -1,12 +1,12 @@
 import json
-from typing import Any
+from typing import Any, Dict
 
 
 class FileSink:
     def __init__(self, path: str):
         self._file = open(path, "w")
 
-    def write(self, item: dict[str, Any]) -> None:
+    def write(self, item: Dict[str, Any]) -> None:
         self._file.write(json.dumps(item, ensure_ascii=False, indent=2) + "\n")
         # self._file.write(json.dumps(item, ensure_ascii=False) + "\n")
 
