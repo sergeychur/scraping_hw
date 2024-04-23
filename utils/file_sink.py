18f4a6ef22611aceeda00282705dc3ac48f28689
Maksim <Maksim>
2024-04-24 00:18:04 +0300

    Pretty json removed for decoding

--- utils/file_sink.py
+++ utils/file_sink.py
@@ -7,7 +7,7 @@ class FileSink:
         self._file = open(path, "w")
 
     def write(self, item: Dict[str, Any]) -> None:
-        self._file.write(json.dumps(item, ensure_ascii=False, indent=2) + "\n")
+        self._file.write(json.dumps(item, ensure_ascii=False) + "\n")
         # self._file.write(json.dumps(item, ensure_ascii=False) + "\n")
 
     def __del__(self):
