b0ada01773eeb0f4a6493fe8f0f8f962cdfa3c93
Ekaterina Selyankina <selyankina@ispras.ru>
2024-04-24 14:46:36 +0300

    fix rate limiter

--- runner/simple_rate_limiter.py
+++ runner/simple_rate_limiter.py
@@ -6,13 +6,12 @@ class SimpleRateLimiter:
     def __init__(self, rate):
         self._lock = threading.Lock()
         self._delta = 1. / rate
-        self._last_called_ts = time.time()
+        self._last_called_ts = 0
 
     def get_delay(self, now=None):
         with self._lock:
             if now is None:
                 now = time.time()
-            expected_next_call_ts = self._last_called_ts + self._delta
-            delay = max(expected_next_call_ts - now, 0)
-            self._last_called_ts = max(expected_next_call_ts, now)
-            return delay
+            expected_next_call_ts = max(self._last_called_ts + self._delta, now)
+            self._last_called_ts = expected_next_call_ts
+            return max(expected_next_call_ts - now, 0)
