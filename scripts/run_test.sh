2c977d45c641607d0f2582bcd3d02aa9aeddb25d
Sergey Churikov <s.churikov@corp.mail.ru>
2024-09-22 19:42:21 +0300

    Updated task to be more automated

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -2,10 +2,17 @@
 
 set -eu
 
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 <path to local folder>"
+    exit 1
+fi
+
+local_folder=$1
+
 mkdir ./real_results/
 echo "Running parser implementation "
 cur_user=$(whoami)
-timeout 1200 ./scripts/run.sh http://localhost/wiki/Чемпионат_Европы_по_футболу_2024 ./real_results/result.jsonl
+timeout --verbose 120 ./scripts/run.sh http://localhost/wiki/Чемпионат_Европы_по_футболу_2024 ./real_results/result.jsonl
 echo "Assessing results"
-python ./scripts/compare_results.py ./test_data/result.jsonl ./real_results/result.jsonl
+python ./scripts/compare_results.py ${local_folder}/result.jsonl ./real_results/result.jsonl
 echo "OK"
