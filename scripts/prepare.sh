57c2a7d123ad4f81c049d5bd9697e6823a9258d8
Sergey Churikov <s.churikov@corp.mail.ru>
2024-09-22 19:42:21 +0300

    Updated task to be more automated

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,7 +4,7 @@ set -eu
 
 # TODO: place real preparations for your parser here
 
-python3.8 -m venv venv
+python3.12 -m venv venv
 . venv/bin/activate
 
 pip install -r requirements.txt
--- scripts/setup_nginx.sh
+++ scripts/setup_nginx.sh
@@ -2,11 +2,17 @@
 
 set -eu
 
+if [[ $# != 1 ]]; then
+    echo "Usage: $0 <path to local folder>"
+    exit 1
+fi
+
 data_folder=/var/www/wiki_data/wiki
+local_folder=$(realpath $1)
 
 apt install nginx
 mkdir -p $data_folder
-cp test_data/*.html $data_folder
+tar xvf $local_folder/htmls.tar.gz -C $data_folder
 rm /etc/nginx/sites-available/* /etc/nginx/sites-enabled/*
 cp scripts/wiki_nginx.conf /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/wiki_nginx.conf /etc/nginx/sites-enabled/
